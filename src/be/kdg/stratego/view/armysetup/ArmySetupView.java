ec92c1d2f9fb3045a8a398eddbc0e320a88c3c0e
JonasTheGamer <38383712+JonasTheGamer@users.noreply.github.com>
2022-03-01 20:57:42 +0100

    Finish newgame structure & style & do not commit workspace

--- src/be/kdg/stratego/view/armysetup/ArmySetupView.java
+++ src/be/kdg/stratego/view/armysetup/ArmySetupView.java
@@ -1,26 +1,30 @@
 package be.kdg.stratego.view.armysetup;
 
+import be.kdg.stratego.view.Backgrounds;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class ArmySetupView extends BorderPane {
     // Controls
-    private Label lblScreenTitle = new Label("Player 1: Place your army");
-    private Label lblSetupTitle = new Label("Setup");
-    private Button btnLoad = new Button("Load");
-    private Button btnSave = new Button("Save");
+    private Label lblScreenTitle;
+    private Label lblSetupTitle ;
+    private Button btnLoad;
+    private Button btnSave;
 
-    private Label lblGameTitle = new Label("Game");
-    private Button btnStart = new Button ("Start");
-    private Button btnBack = new Button ("Back");
+    private Label lblGameTitle;
+    private Button btnStart;
+    private Button btnBack;
 
     // Panes
-    private GridPane gpPieces = new GridPane(); 
-    private GridPane gpField = new GridPane();
-    private HBox hbMain = new HBox();
-    private VBox vbMenu = new VBox();
-    private VBox vbSetup = new VBox();
-    private VBox vbGame = new VBox();
+    private GridPane gpPieces;
+    private GridPane gpField;
+    private VBox vbSetup;
+    private VBox vbMenu;
+    private VBox vbGame;
 
     public ArmySetupView() {
         InitializeNodes();
@@ -29,10 +33,16 @@ public class ArmySetupView extends BorderPane {
 
     public void InitializeNodes() {
         // Controls
+
+        //// Header
         lblScreenTitle = new Label("Player 1: Place your army");
+
+        //// Menu setup
         lblSetupTitle = new Label("Setup");
         btnLoad = new Button("Load");
         btnSave = new Button("Save");
+
+        //// Menu game
         lblGameTitle = new Label("Game");
         btnStart = new Button("Start");
         btnBack = new Button("Back");
@@ -40,33 +50,96 @@ public class ArmySetupView extends BorderPane {
         // Panes
         gpPieces = new GridPane(); 
         gpField = new GridPane();
-        hbMain = new HBox();
         vbMenu = new VBox();
         vbSetup = new VBox();
         vbGame = new VBox();
 
-        // The gridpane will be filled in the presenter class with all pieces that are available.
-        // The field will also be filled with the question marks and the pieces in the presenter, to allow for a dynamic map size. (x rows & x columns)
+        // The gridpane gpPieces will be filled in the presenter class with all pieces that are available.
+        // The gridpane gpField will also be filled with the question marks and the pieces in the presenter, to allow for a dynamic map size. (x rows & x columns)
 
     }
 
     public void LayoutNodes() {
         // Styling variables
+        double btnWidth = 400;
+        double btnHeight = 50;
+        Background btnBackground = new Background(new BackgroundFill(
+                new Color(0, 0, 0.2, 0.9),
+                new CornerRadii(20),
+                null
+        ));
 
-        // Styling controls
+        // Main borderpane
+        this.setBackground(Backgrounds.background);
 
-        // Styling panes
-        
+        // Header (title)
 
-        // Add to panes
-        this.setTop(lblGameTitle);
-        this.setCenter(hbMain);
+        //// Title (player X, place your army)
+        lblScreenTitle.setFont(Font.font("Verdana", 50));
+        this.setAlignment(lblScreenTitle, Pos.CENTER);
+        this.setMargin(lblScreenTitle, new Insets(15,0,0,0));
 
-        hbMain.getChildren().addAll(gpPieces, gpField, vbMenu);
-        vbMenu.getChildren().addAll(vbSetup, vbGame);
+        this.setTop(lblScreenTitle);
+
+        //// Army pieces
+        ////// The gridpane will be filled in the presenter class with all pieces that are available.
+        this.setLeft(gpPieces);
+
+        //// Field
+        ////// The field will also be filled with the question marks and the pieces in the presenter, to allow for a dynamic map size. (x rows & x columns)
+       this.setCenter(gpField);
+
+        //// Menu
+        vbMenu.setBackground(Backgrounds.boxBackground);
+        vbMenu.setAlignment(Pos.CENTER);
+        vbMenu.setPrefWidth(350);
+        vbMenu.setSpacing(50);
+        this.setMargin(vbMenu, new Insets(25, 75,50,0));
+        this.setRight(vbMenu);
+
+        ////// Setup buttons (load & save)
+        lblSetupTitle.setFont(Font.font("Verdana", 15));
+        lblSetupTitle.setTextFill(Color.WHITE);
+
+        vbSetup.setAlignment(Pos.CENTER);
+        vbSetup.setSpacing(20);
+
+        btnLoad.setPrefWidth(btnWidth);
+        btnLoad.setPrefHeight(btnHeight);
+        btnLoad.setTextFill(Color.WHITE);
+        btnLoad.setBackground(btnBackground);
+
+        btnSave.setPrefWidth(btnWidth);
+        btnSave.setPrefHeight(btnHeight);
+        btnSave.setTextFill(Color.WHITE);
+        btnSave.setBackground(btnBackground);
 
         vbSetup.getChildren().addAll(lblSetupTitle, btnLoad, btnSave);
+        vbMenu.getChildren().add(vbSetup);
+
+        ////// Game buttons (start & back)
+        lblGameTitle.setFont(Font.font("Verdana", 15));
+        lblGameTitle.setTextFill(Color.WHITE);
+
+        vbGame.setAlignment(Pos.CENTER);
+        vbGame.setSpacing(20);
+
+        btnStart.setPrefWidth(btnWidth);
+        btnStart.setPrefHeight(btnHeight);
+        btnStart.setTextFill(Color.WHITE);
+        btnStart.setBackground(btnBackground);
+
+        btnBack.setPrefWidth(btnWidth);
+        btnBack.setPrefHeight(btnHeight);
+        btnBack.setTextFill(Color.WHITE);
+        btnBack.setBackground(btnBackground);
+
         vbGame.getChildren().addAll(lblGameTitle, btnStart, btnBack);
+        vbMenu.getChildren().add(vbGame);
+
+        // Add to panes
+
+
     }
 
 }
