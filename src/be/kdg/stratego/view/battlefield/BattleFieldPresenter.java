4e749aa7d3fede5791088bcd462134c45df2d354
Liam <liam.verschueren@student.kdg.be>
2022-03-15 11:48:59 +0100

    Tjdelijk kapot poesje

--- src/be/kdg/stratego/view/battlefield/BattleFieldPresenter.java
+++ src/be/kdg/stratego/view/battlefield/BattleFieldPresenter.java
@@ -29,72 +29,6 @@ public class BattleFieldPresenter {
     private MovingPiece selectedPiece;
     private HashSet<GameBoardField> allowedMoves;
 
-    private final EventHandler onFieldClick = new EventHandler<MouseEvent>() {
-        @Override
-        public void handle(MouseEvent mouseEvent) {
-            // Find out which of the fields actually got pressed. All we need to know is the coordinates.
-            StackPane clickedPane = (StackPane) mouseEvent.getSource();
-            String clickedId = clickedPane.getId();
-
-            String[] idData = clickedId.split("-");
-
-            int posX = Integer.parseInt(idData[0]);
-            int posY = Integer.parseInt(idData[1]);
-
-            // Now, figure out the field object
-            GameBoardField field = model.getGameBoard().getGameBoardField(posX, posY);
-
-            // UNSELECT - Check if player clicked on the currently selected piece
-            if(field.isOccupied() && field.getPiece().equals(selectedPiece)) {
-                // Clear the selection
-                clearSelection();
-
-                // Update view
-                updateView();
-                return;
-            }
-
-            // SELECT - Did the player click on a piece that's one of their own?
-            if (field.isOccupied() && field.getPiece().getPlayer().equals(currentPlayer)) {
-                // They probably want to move this piece
-                Piece piece = field.getPiece();
-
-                // But let's check if it's movable
-                if(piece instanceof MovingPiece) {
-                    // Select it for movement!
-                    selectedPiece = (MovingPiece) piece;
-
-                    // Get the allowed moves
-                    allowedMoves = model.getGameBoard().getAllowedMoves(selectedPiece);
-
-                    // Highlight them
-                    model.getGameBoard().highLightAllowedMoves(selectedPiece);
-
-                    updateView();
-
-                    return;
-                } else {
-                    // Ignore their request
-                    return;
-                }
-            }
-
-            // MOVE - Did the player click in one of the allowed fields (allowedMoves)?
-            if(allowedMoves.contains(field)) {
-                MovingPiece piece = (MovingPiece) selectedPiece;
-
-                // Clear the selection
-                clearSelection();
-
-                // Move the field
-                piece.moveTo(field);
-
-                // Update the view
-                updateView();
-            }
-        }
-    };
-
     public BattleFieldPresenter(ProgrammaModel model, BattleFieldView view) {
         this.model = model;
         this.view = view;
@@ -112,24 +46,73 @@ public class BattleFieldPresenter {
         this.updateView();
     }
 
-    public void clearSelection() {
-        // Unhighlight
-        model.getGameBoard().unHighlightAllFields();
-
-        // Unselect
-        selectedPiece = null;
-        allowedMoves.clear();
-    }
     private void addEventHandlers() {
+        GameBoardField[][] fields = model.getGameBoard().getGameBoardFields();
 
+        for (GameBoardField[] fieldColumn : fields) {
+            for (GameBoardField field : fieldColumn) {
+                StackPane fieldPane = field.getPane();
+
+                fieldPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        //Liam: Model
+
+                        // UNSELECT - Check if player clicked on the currently selected piece
+                        if (field.isOccupied() && field.getPiece().equals(selectedPiece)) {
+                            // Clear the selection
+                            clearSelection();
+
+                            // Update view
+                            updateView();
+                            return;
+                        }
+
+                        // SELECT - Did the player click on a piece that's one of their own?
+                        if (field.isOccupied() && field.getPiece().getPlayer().equals(currentPlayer)) {
+                            // They probably want to move this piece
+                            Piece piece = field.getPiece();
+
+                            // But let's check if it's movable
+                            if (piece instanceof MovingPiece) {
+                                // Select it for movement!
+                                selectedPiece = (MovingPiece) piece;
+
+                                // Get the allowed moves
+                                allowedMoves = model.getGameBoard().getAllowedMoves(selectedPiece);
+
+                                // Highlight them
+                                model.getGameBoard().highLightAllowedMoves(selectedPiece);
+
+                                updateView();
+
+                                return;
+                            } else {
+                                // Ignore their request
+                                return;
+                            }
+                        }
+
+                        // MOVE - Did the player click in one of the allowed fields (allowedMoves)?
+                        if (allowedMoves.contains(field)) {
+                            MovingPiece piece = (MovingPiece) selectedPiece;
+
+                            // Clear the selection
+                            clearSelection();
+
+                            // Move the field
+                            piece.moveTo(field);
+
+                            // Update the view
+                            updateView();
+                        }
+                    }
+                });
+            }
+        }
     }
 
     private void updateView() {
-        // Initialize variables
-        int boardWidth = model.getGameBoard().getGrootteX();
-        int boardHeight = model.getGameBoard().getGrootteY();
-        int amountOfRowsPerPlayer = (model.getGameBoard().getGrootteY() - 2) / 2;
-
         // Game board
         //// Empty game board gridpane
         view.getGpBoard().getChildren().clear();
@@ -140,14 +123,22 @@ public class BattleFieldPresenter {
         for (GameBoardField[] fieldColumn : fields) {
             for (GameBoardField field : fieldColumn) {
                 StackPane fieldPane = field.getPane();
-                fieldPane.setOnMouseClicked(onFieldClick);
                 view.getGpBoard().add(fieldPane, field.getPositionX(), field.getPositionY());
             }
         }
     }
 
-    public void addWindowEventHandlers() {
+    private void addWindowEventHandlers() {
 
     }
 
+
+    private void clearSelection() {
+        // Unhighlight
+        model.getGameBoard().unHighlightAllFields();
+
+        // Unselect
+        selectedPiece = null;
+        allowedMoves.clear();
+    }
 }
