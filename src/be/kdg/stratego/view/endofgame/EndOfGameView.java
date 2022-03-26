612b9ff49078801ad1c976811b0a6f9b9a53c0a6
JonasTheGamer <38383712+JonasTheGamer@users.noreply.github.com>
2022-03-26 18:01:35 +0100

    Start making EndOfGamePresenter and EndOfGameView

---
+++ src/be/kdg/stratego/view/endofgame/EndOfGameView.java
@@ -0,0 +1,18 @@
+package be.kdg.stratego.view.endofgame;
+
+import javafx.scene.layout.VBox;
+
+public class EndOfGameView extends VBox {
+
+    public EndOfGameView() {
+
+    }
+
+    public void initialiseNodes() {
+
+    }
+
+    public void layoutNodes() {
+
+    }
+}
